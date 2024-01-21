ac758137952ce18a7688b158b1c0e990358d9362
Luis Arias <luis.arias@instaleap.io>
2024-01-21 13:05:55 -0500

    remove unused multithread

--- src/input_output/console.rs
+++ src/input_output/console.rs
@@ -1,5 +1,4 @@
 use std::io::{stdin, stdout, Write};
-use std::thread;
 use termion::event::{Event, Key};
 use termion::input::TermRead;
 use termion::raw::{IntoRawMode, RawTerminal};
@@ -24,7 +23,7 @@ impl Console {
         Self {}
     }
 
-    pub fn start(&mut self, path: String, cols: usize) -> thread::JoinHandle<()> {
+    pub fn start(&mut self, path: String, cols: usize) {
         let indexed_mesh = read_mesh(path).unwrap();
         let object = Object::new(indexed_mesh.clone());
         let maximum_diameter = 2.0 * object.get_maximum_radius();
@@ -34,48 +33,46 @@ impl Console {
 
         let mut controller = Controller::new(scene, matrix);
 
-        thread::spawn(move || {
-            let stdin = stdin();
-            let mut stdout = stdout().into_raw_mode().unwrap();
+        let stdin = stdin();
+        let mut stdout = stdout().into_raw_mode().unwrap();
 
-            Self::print_frame(
-                "To Start, press enter. To rotate the object, use the arrow keys. To quit, press 'q'.".to_string(),
+        Self::print_frame(
+                "To start press Enter key.\r\nTo rotate the object around XY axis use arrow keys.\r\nTo quit, press 'q'.".to_string(),
                 &mut stdout,
             );
-            stdout.flush().unwrap();
+        stdout.flush().unwrap();
 
-            for c in stdin.events() {
-                let evt = c.unwrap();
-                match evt {
-                    Event::Key(Key::Char('q')) => {
-                        Self::print_frame("Goodbye!\r\n".to_string(), &mut stdout);
-                        break;
-                    }
-                    Event::Key(Key::Char('\n')) => {
-                        let frame = controller.enter_key();
-                        Self::print_frame(frame, &mut stdout);
-                    }
-                    Event::Key(Key::Up) => {
-                        let frame = controller.up_key();
-                        Self::print_frame(frame, &mut stdout);
-                    }
-                    Event::Key(Key::Down) => {
-                        let frame = controller.down_key();
-                        Self::print_frame(frame, &mut stdout);
-                    }
-                    Event::Key(Key::Left) => {
-                        let frame = controller.left_key();
-                        Self::print_frame(frame, &mut stdout);
-                    }
-                    Event::Key(Key::Right) => {
-                        let frame = controller.right_key();
-                        Self::print_frame(frame, &mut stdout);
-                    }
-                    _ => {}
+        for c in stdin.events() {
+            let evt = c.unwrap();
+            match evt {
+                Event::Key(Key::Char('q')) => {
+                    Self::print_frame("Goodbye!\r\n".to_string(), &mut stdout);
+                    break;
                 }
-                stdout.flush().unwrap();
+                Event::Key(Key::Char('\n')) => {
+                    let frame = controller.enter_key();
+                    Self::print_frame(frame, &mut stdout);
+                }
+                Event::Key(Key::Up) => {
+                    let frame = controller.up_key();
+                    Self::print_frame(frame, &mut stdout);
+                }
+                Event::Key(Key::Down) => {
+                    let frame = controller.down_key();
+                    Self::print_frame(frame, &mut stdout);
+                }
+                Event::Key(Key::Left) => {
+                    let frame = controller.left_key();
+                    Self::print_frame(frame, &mut stdout);
+                }
+                Event::Key(Key::Right) => {
+                    let frame = controller.right_key();
+                    Self::print_frame(frame, &mut stdout);
+                }
+                _ => {}
             }
-        })
+            stdout.flush().unwrap();
+        }
     }
 
     fn print_frame(frame: String, stdout: &mut RawTerminal<std::io::Stdout>) {
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ use sx3d::input_output::{get_file_path, Console};
 fn main() {
     let (path, cols) = get_file_path();
 
-    let mut keyboard_input = Console::new();
-    let handle = keyboard_input.start(path, cols);
-    handle.join().unwrap();
+    let mut console = Console::new();
+    console.start(path, cols)
 }
