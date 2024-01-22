3ade42a2cf237d309248e73a95fea52c2cddb6f9
Luis Arias <luis.arias@instaleap.io>
2024-01-22 09:44:03 -0500

    Removing duplicated code.

--- src/controller/controller.rs
+++ src/controller/controller.rs
@@ -14,40 +14,32 @@ impl Controller {
     }
 
     fn get_frame(&self) -> String {
+        let visible_mesh = self.scene.get_visible_mesh();
+        self.matrix.project(&visible_mesh);
         self.matrix.get_frame()
     }
 
     pub fn enter_key(&mut self) -> String {
-        let visible_mesh = self.scene.get_visible_mesh();
-        self.matrix.project(&visible_mesh);
         self.get_frame()
     }
 
     pub fn up_key(&mut self) -> String {
         self.scene.rotate_delta_x(&-DEFAULT_DELTA);
-        let visible_mesh = self.scene.get_visible_mesh();
-        self.matrix.project(&visible_mesh);
         self.get_frame()
     }
 
     pub fn down_key(&mut self) -> String {
         self.scene.rotate_delta_x(&DEFAULT_DELTA);
-        let visible_mesh = self.scene.get_visible_mesh();
-        self.matrix.project(&visible_mesh);
         self.get_frame()
     }
 
     pub fn left_key(&mut self) -> String {
         self.scene.rotate_delta_y(&-DEFAULT_DELTA);
-        let visible_mesh = self.scene.get_visible_mesh();
-        self.matrix.project(&visible_mesh);
         self.get_frame()
     }
 
     pub fn right_key(&mut self) -> String {
         self.scene.rotate_delta_y(&DEFAULT_DELTA);
-        let visible_mesh = self.scene.get_visible_mesh();
-        self.matrix.project(&visible_mesh);
         self.get_frame()
     }
 }
